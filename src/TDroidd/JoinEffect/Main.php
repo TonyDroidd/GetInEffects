df8f7fe7245a0385d1d5affd7ddc5bcb4a6f2120
70NYDR011D <tonydroidd@gmail.com>
2015-04-20 15:12:42 -0600

    Update Main.php

--- src/TDroidd/JoinEffect/Main.php
+++ src/TDroidd/JoinEffect/Main.php
@@ -19,23 +19,15 @@ class Main extends PluginBase implements Listener {
 	 */	
 	public function onEnable(){
 	        $this->saveDefaultConfig();
-			@mkdir($this->getDataFolder());
-			
-			if(!file_exists($this->getDataFolder() . "config.yml")){
-				file_put_contents($this->getDataFolder() . "config.yml",yaml_emit(array()));
-				}
-			$this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, [
-				"EffectID" => "1",
-			]);
+			$c = yaml_parse(file_get_contents($this->getDataFolder() . "config.yml"));
+			$this->effect = $c["EffectID"];
                 $this->getLogger()->info(TextFormat::RED . "JoinEffect By TDroidd  0.1 Enabled!");
 
 }
 
 	public function onJoin(PlayerJoinEvent $event) {
-		$p = $event->getPlayer();
-		    $EffectID = $this->getConfig()->getAll();
-			$EffectID = yaml_parse(file_get_contents($this->getDataFolder() . "config.yml"));
-			$effect = Effect::getEffect($EffectID); //Effect ID
+			$p = $event->getPlayer();
+			$effect = Effect::getEffect($this->effect = $c["EffectID"]); //Effect ID
 			$effect->setVisible(false); //Particles
 			$effect->setDuration(1200); //Ticks
 			$p->addEffect($effect);
