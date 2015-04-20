7a4b69136f1e931b95d41aeb708dc36fa5c4e30c
70NYDR011D <tonydroidd@gmail.com>
2015-04-20 14:59:09 -0600

    Added config folder

--- src/TDroidd/JoinEffect/Main.php
+++ src/TDroidd/JoinEffect/Main.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace TDroidd\JoinEffect;
 use pocketmine\Player;
 use pocketmine\entity\Effect;
@@ -7,6 +8,7 @@ use pocketmine\event\Listener;
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
 use pocketmine\event\player\PlayerJoinEvent;
+
 class Main extends PluginBase implements Listener {
 	/**
 	 * OnEnable
@@ -17,10 +19,18 @@ class Main extends PluginBase implements Listener {
 	 */	
 	public function onEnable(){
 	        $this->saveDefaultConfig();
-		$EffectID = yaml_parse(file_get_contents($this->getDataFolder() . "config.yml"));
-		$this->Effect = array($EffectID["EffectID"]);
+			@mkdir($this->getDataFolder());
+			
+			if(!file_exists($this->getDataFolder() . "config.yml")){
+				file_put_contents($this->getDataFolder() . "config.yml",yaml_emit(array()));
+				}
+			$this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, [
+				"EffectID" => "1",
+			]);
                 $this->getLogger()->info(TextFormat::RED . "JoinEffect By TDroidd  0.1 Enabled!");
+
 }
+
 	public function onJoin(PlayerJoinEvent $event) {
 		$p = $event->getPlayer();
 		    $EffectID = $this->getConfig()->getAll();
@@ -28,7 +38,7 @@ class Main extends PluginBase implements Listener {
 			$effect = Effect::getEffect($EffectID); //Effect ID
 			$effect->setVisible(false); //Particles
 			$effect->setDuration(1200); //Ticks
-			$p->addEffect($EffectID);
+			$p->addEffect($effect);
 	}
 	/**
 	 * OnDisable
@@ -38,6 +48,6 @@ class Main extends PluginBase implements Listener {
 	 */
 	public function onDisable() {
 		$this->save ();
-		$this->getLogger()->info(TextFormat::RED . "JoinEffect By TDroidd  0.1 Unloaded!");
+		$this->getLogger()->info(TextFormat::RED . "JoinEffect By TDroidd 0.1 Unloaded!");
 	}
 }
