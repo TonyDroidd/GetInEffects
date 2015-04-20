92860c71c08038c72948ec655a436f6bfb067550
70NYDR011D <tonydroidd@gmail.com>
2015-04-19 22:11:08 -0600

    Update main.php

--- src/TDroidd/JoinEffect/main.php
+++ src/TDroidd/JoinEffect/main.php
@@ -13,12 +13,16 @@ class Main extends PluginBase{
 	public function onEnable(){
 		 $this->saveDefaultConfig();
 		$EffectID = yaml_parse(file_get_contents($this->getDataFolder() . "config.yml"));
+		$this->Effect = array($EffectID["EffectID"]);
+
 }
 	public function onJoin(PlayerJoinEvent $event) {
 		$p = $event->getPlayer();
+		    $EffectID = $this->getConfig()->getAll();
+			$EffectID = yaml_parse(file_get_contents($this->getDataFolder() . "config.yml"));
 			$effect = Effect::getEffect($EffectID); //Effect ID
 			$effect->setVisible(false); //Particles
 			$effect->setDuration(1200); //Ticks
-			$p->addEffect($effect);
+			$p->addEffect($EffectID);
 	}
 }
