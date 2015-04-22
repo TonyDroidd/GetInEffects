626f56ed01426dcc86954b6f8a740a8fcf0b0768
70NYDR011D <tonydroidd@gmail.com>
2015-04-22 17:51:12 -0600

    Fixed ALL bugs!

--- src/TDroidd/JoinEffect/Main.php
+++ src/TDroidd/JoinEffect/Main.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace TDroidd\JoinEffect;
 use pocketmine\Player;
 use pocketmine\entity\Effect;
@@ -8,7 +7,7 @@ use pocketmine\event\Listener;
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
 use pocketmine\event\player\PlayerJoinEvent;
-
+use pocketmine\event\player\PlayerRespawnEvent;
 class Main extends PluginBase implements Listener {
 	/**
 	 * OnEnable
@@ -21,14 +20,14 @@ class Main extends PluginBase implements Listener {
 		$this->saveDefaultConfig();
 		$cfg = yaml_parse(file_get_contents($this->getDataFolder() . "config.yml"));
 		$this->effect = array($cfg["Effect-ID"]);
+		$cfg = $this->getConfig()->getAll();
 		$this->getServer()->getPluginManager()->registerEvents($this,$this);
-        	$this->getLogger()->info(TextFormat::RED . "JoinEffect By TDroidd  0.1 Enabled!");
-
+		$this->getLogger()->info(TextFormat::RED . "JoinEffect By TDroidd  0.1 Enabled!");
 }
-
-	public function onSpawn(PlayerRespawnEvent $event) {
+		public function onSpawn(PlayerRespawnEvent $event) {
 		$p = $event->getPlayer();
-		$effect = Effect::getEffect($this->getConfig()->get("Effect-ID")); //Effect ID
+		$cfg = $this->getConfig()->getAll();
+		$effect = Effect::getEffect($this->effect = $cfg["Effect-ID"]); //Effect ID
 		$effect->setVisible(false); //Particles
 		$effect->setDuration(1200); //Ticks
 		$p->addEffect($effect);
