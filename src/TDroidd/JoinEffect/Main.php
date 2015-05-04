e84e91803fcead34a4ff44b273de8e83b1e333ff
70NYDR01DD <tonydroidd@gmail.com>
2015-05-04 14:34:45 -0600

    Update Main.php

--- src/TDroidd/JoinEffect/Main.php
+++ src/TDroidd/JoinEffect/Main.php
@@ -34,7 +34,7 @@ class Main extends PluginBase implements Listener {
 		$effect->setAmplifier($this->amplifier = $cfg["Amplifier"]);
 		$effect->setDuration($this->duration = $cfg["Duration"]); //Ticks
 		$p->addEffect($effect);
-		}
+	}
 	/**
 	 * OnDisable
 	 * (non-PHPdoc)
