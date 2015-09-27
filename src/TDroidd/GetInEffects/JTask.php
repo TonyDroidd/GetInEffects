5bd0a4f2f84ea8286a62a9341da09fbc0bce44b9
Victor T. <pro_victor@ymail.com>
2015-09-26 17:00:13 -0700

    Update JTask.php

--- src/TDroidd/GetInEffects/JTask.php
+++ src/TDroidd/GetInEffects/JTask.php
@@ -1,21 +1,18 @@
 <?php
-
 namespace TDroidd\GetInEffects;
 use pocketmine\scheduler\Task;
 class JTask extends Task{
-
-	protected $callable;
-	protected $args;
-	public function __construct(callable $callable, array $args = []){
-		$this->callable = $callable;
-		$this->args = $args;
-		$this->args[] = $this;
-	}
-	public function getCallable(){
-		return $this->callable;
-	}
-	public function onRun($currentTicks){
-		call_user_func_array($this->callable, $this->args);
-	}
-
+    protected $callable;
+    protected $args;
+    public function __construct(callable $callable, array $args = []){
+	$this->callable = $callable;
+	$this->args = $args;
+	$this->args[] = $this;
+    }
+    public function getCallable(){
+	return $this->callable;
+    }
+    public function onRun($currentTick){
+	call_user_func_array($this->callable, $this->args);
+    }
 }
